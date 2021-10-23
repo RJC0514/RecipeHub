62342dce5e82ff4c28d41e99d7ff1b880ee1bd6d
tylcos <tylcos@users.noreply.github.com>
2021-10-23 13:42:12 -0400

    Fix database dump

--- django_project/core/models.py
+++ django_project/core/models.py
@@ -12,4 +12,4 @@ class Recipe(models.Model):
 class RecipeIngredients(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    name=models.CharField(max_length=50)
+    measurement=models.CharField(max_length=50)
