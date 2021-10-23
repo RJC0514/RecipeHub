2ccaf8a332ea6485b4ee6af451ff95b6ddc94ce7
guy <shaun@shitchell.com>
2021-10-22 22:35:15 -0400

    added ingredient & recipe models

--- django_project/core/models.py
+++ django_project/core/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=25)
 
-# class TestModel(models.Model):
-#
-# 	name = models.CharField(max_length=25, unique=True, help_text='Test')
-# 	enabled = models.BooleanField(default=False, help_text='Enabled')
-#
-# 	class Meta:
-# 		ordering = ['name']
-#
-# 	def __str__(self):
-# 		return self.name
+class Recipe(models.Model):
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredients')
+    instructions = models.TextField()
+
+class RecipeIngredients(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
